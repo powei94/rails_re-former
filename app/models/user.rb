7ab3fc04254d19693f7ccbb3af10db2a9b98a3b7
Cristóbal <cristobal.tbecerraf@gmail.com>
2020-04-25 15:38:13 -0400

    detección de errores en validaciones de active record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: {with: URI::MailTo::EMAIL_REGEXP}
     validates :password, presence: true
 end
